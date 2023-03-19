5e9f9beafc1948a119120f621e3178c46b16e2a3
ma-curle <adi@gmail.com>
2023-03-19 10:51:15 +0545

    created product model

--- inventorypro/dashboard/models.py
+++ inventorypro/dashboard/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+CATEGORY = (
+    ('stationaries','stationaries'),
+    ('Food','Food'),
+    ('Electronics','Electronics'),
+    ('Household','Household'),
+  )
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    quantity = models.PositiveIntegerField()
