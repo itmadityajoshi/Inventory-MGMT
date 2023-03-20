8c1082d276e9ad6dadd5ccce8d56c283c40eb626
ma-curle <adi@gmail.com>
2023-03-20 09:11:52 +0545

    change title of admin site in db

--- inventorypro/dashboard/models.py
+++ inventorypro/dashboard/models.py
@@ -11,6 +11,11 @@ CATEGORY = (
   )
 
 class Product(models.Model):
-    name = models.CharField(max_length=50)
-    category = models.CharField(max_length=50)
-    quantity = models.PositiveIntegerField()
+    name = models.CharField(max_length=50, null=True)
+    category = models.CharField(max_length=50, choices=CATEGORY, null=True)
+    quantity = models.PositiveIntegerField(null=True)
+
+
+    def __str__(self):
+        return f"{self.name} - {self.quantity}"
+    
